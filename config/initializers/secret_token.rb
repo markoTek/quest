23938906a5f84b3ec63ce910a5d8f8eae9ee732b
Markus <pomline@posteo.de>
2014-01-02 23:57:03 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quest::Application.config.secret_key_base = '05cb9985a49b910b974bf2262670c0191a434957f4f3b862d1e125a58f1ae0ac4e9df618838eca84eb695009988ee72fcd52bd234f41e7cf2462f86a9765f985'
